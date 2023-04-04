59a3312e3df63fa30de15fc1974991a2fef2ea74
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-04-04 09:01:04 +0700

    add build script

---
+++ scripts/buildAll.ps1
@@ -0,0 +1,19 @@
+$env:Path += ';C:\gstreamer\1.22.0\msvc_x86_64\bin'
+$env:PKG_CONFIG_PATH = "C:\gstreamer\1.22.0\msvc_x86_64\lib\pkgconfig"
+
+git submodule update --init --recursive
+
+# build GO 
+go build  -o daemon.exe ./cmd/
+
+Set-Location .\hub
+go build -o hub.exe  ./cmd/server/
+Set-Location ../
+
+# build .NET
+Set-Location .\hid
+dotnet build . --output "bin" --self-contained true --runtime win-x64
+Set-Location ..
+
+
+Compress-Archive . -DestinationPath .\thinkremote.zip 
