40e0ae512bad8a8ed94632def2a2bcbd83571228
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-04-05 21:36:45 -0700

    do wait

--- scripts/download.ps1
+++ scripts/download.ps1
@@ -16,6 +16,6 @@ $git.Downloadfile("https://github.com/git-for-windows/git/releases/download/v2.4
 ./msi/gcc.exe
 ./msi/git.exe
 
-./msi/go.msi /qb                      
-./msi/gstreamer-dev.msi /qb           
-./msi/gstreamer.msi /qb            
+Start-Process ./msi/go.msi  -ArgumentList "/qn" -Wait                      
+Start-Process ./msi/gstreamer-dev.msi  -ArgumentList "/qn" -Wait           
+Start-Process ./msi/gstreamer.msi  -ArgumentList "/qn" -Wait            
