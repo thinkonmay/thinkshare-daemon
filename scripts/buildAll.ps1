8744830afe85d73089ae62333f22f8fb2af1acc6
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2023-04-10 20:43:41 +0700

    Update buildAll.ps1

--- scripts/buildAll.ps1
+++ scripts/buildAll.ps1
@@ -1,6 +1,3 @@
-$env:Path += ';C:\gstreamer\1.22.0\msvc_x86_64\bin'
-$env:PKG_CONFIG_PATH = "C:\gstreamer\1.22.0\msvc_x86_64\lib\pkgconfig"
-
 git clone "https://github.com/thinkonmay/thinkshare-daemon" daemon
 git checkout master
 Set-Location .\daemon
@@ -18,6 +15,3 @@ Set-Location ../
 Set-Location .\hid
 dotnet build . --output "bin" --self-contained true --runtime win-x64
 Set-Location ..
-
-
-Compress-Archive . -DestinationPath .\thinkremote.zip 
