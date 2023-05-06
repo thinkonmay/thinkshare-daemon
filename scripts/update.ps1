a278fb77db63917b07df4b377106bb1742289c94
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-05-06 11:07:53 +0700

    update script

---
+++ scripts/update.ps1
@@ -0,0 +1,15 @@
+git checkout master
+git submodule update --init --recursive
+
+# build GO 
+go build  -o daemon.exe ./cmd/daemon/
+go build  -o cli.exe ./cmd/cli/
+
+Set-Location .\hub
+go build -o hub.exe  ./cmd/server/
+Set-Location ../
+
+# build .NET
+Set-Location .\hid
+dotnet build . --output "bin" --self-contained true --runtime win-x64
+Set-Location ..
