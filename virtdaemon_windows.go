c91656c82e5c553bdfe6adfe3105786eed4cd519
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-03-30 16:24:21 +0700

    fix build

--- virtdaemon_windows.go
+++ virtdaemon_windows.go
@@ -13,7 +13,7 @@ func QueryInfo(info *packet.WorkerInfor)                     {}
 func InfoBuilder(info packet.WorkerInfor) packet.WorkerInfor { return info }
 
 func (daemon *Daemon) HandleVirtdaemon(*ClusterConfig) {}
-func (daemon *Daemon) DeployVM(g string) (*packet.WorkerInfor, error) {
+func (daemon *Daemon) DeployVM(*packet.WorkerSession) (*packet.WorkerInfor, error) {
 	return nil, fmt.Errorf("window VM not available")
 }
 func (daemon *Daemon) ShutdownVM(*packet.WorkerInfor) error {
@@ -21,4 +21,14 @@ func (daemon *Daemon) ShutdownVM(*packet.WorkerInfor) error {
 }
 func (daemon *Daemon) HandleSessionForward(ss *packet.WorkerSession, command string) (*packet.WorkerSession, error) {
 	return nil, fmt.Errorf("window forward not available")
+}
+
+func (daemon *Daemon) HandleSignaling(token string) (*string, bool) {
+	return nil, false
+}
+func (daemon *Daemon) DeployVMonNode(nss *packet.WorkerSession) (*packet.WorkerSession, error) {
+	return nil, fmt.Errorf("window forward not available")
+}
+func (daemon *Daemon) DeployVMwithVolume(nss *packet.WorkerSession) (*packet.WorkerSession, *packet.WorkerInfor, error) {
+	return nil,nil, fmt.Errorf("window forward not available")
 }
