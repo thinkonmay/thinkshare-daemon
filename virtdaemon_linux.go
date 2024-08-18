0a7c2dbfb2ec3f7f57a6f7a6bda1ba6e19de8f51
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-08-18 04:19:40 +0000

    disable libvirt if network fail

--- virtdaemon_linux.go
+++ virtdaemon_linux.go
@@ -56,6 +56,7 @@ func (daemon *Daemon) HandleVirtdaemon() func() {
 	network, err = libvirt.NewLibvirtNetwork(daemon.cluster.Interface(),daemon.cluster.DNSserver())
 	if err != nil {
 		log.PushLog("failed to start network %s", err.Error())
+		libvirt_available = false
 		return func() {}
 	}
 
